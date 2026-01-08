4fd4100170e3d2ff04a10435c79bbf973606be9c
Daniel S <daniel.b.schuster@gmail.com>
2026-01-08 14:32:36 -0800

    added record_audio() function and simple main() function

--- hardware/src/mic-input.py
+++ hardware/src/mic-input.py
@@ -6,9 +6,10 @@ import soundfile as sf
 
 
 # Recording parameters
-duration = 10
-sample_rate = 16000
-mic_name = "LavMicro"
+DURATION = 10
+SAMPLE_RATE = 16000
+MIC_NAME = "LavMicro" #testing the script will require changing this to match the system microphone
+FILENAME = "mic-output.wav"
 
 
 def get_device(mic_name: str) -> int:
@@ -25,6 +26,7 @@ def get_device(mic_name: str) -> int:
     # Enumerate hardware devices and look for a match with mic_name
     for index, device in enumerate(sd.query_devices()):
         name = device["name"].lower()
+        print(name)
         if mic_name in name and device["max_input_channels"] > 0:
             matches.append((index, device["name"]))
 
@@ -38,6 +40,36 @@ def get_device(mic_name: str) -> int:
         for idx, name in matches:
             print(f"  {idx}: {name}")
         print(f"Using first match: {matches[0][0]}")
-
+    
     return matches[0][0]
 
+
+def record_audio(device: int, duration: int, samplerate: int, output_filename: str):
+    """
+    Records for duration number of seconds, and saves the recorded text at output_filename
+
+    Raises:
+        RuntimeError if an exception occurs when attempting to record
+    """
+
+    try:
+        audio = sd.rec(int(duration * samplerate),
+                    samplerate = samplerate,
+                    channels = 1,
+                    dtype = 'float32',
+                    device = device)
+        sd.wait()
+        sf.write(output_filename, audio, samplerate)
+
+    except Exception as e:
+        raise RuntimeError(f"error: audio recording failed: {e}") from e
+
+
+
+def main():
+    device_index = get_device(MIC_NAME)
+    record_audio(device=device_index, duration=DURATION, samplerate=SAMPLE_RATE, output_filename=FILENAME)
+
+
+if __name__ == "__main__":
+    main()
