d709622a8f267b289529daafc1c19bf8041a0a1f
Satvik Mudgal <satvik.mudgal@gmail.com>
2026-01-10 11:59:57 -0800

    Added comments to IdleScene

--- frontend/electron/renderer/scenes/IdleScene.js
+++ frontend/electron/renderer/scenes/IdleScene.js
@@ -1,6 +1,12 @@
 import * as PIXI from "pixi.js";
 import { FishSwarm, CommonStyles } from "../assets/sprites.js";
 
+/**
+ * This class creates the animations responsible for being displayed
+ * when no user is interacting with the application.
+ * It generates a background, and uses the FishSwarm class from sprites.js to create a swarm of fish.
+ * The swarm class contains the functionality for fish angles and movements and speed.
+ */
 export class IdleScene {
   constructor(app) {
     this.container = new PIXI.Container();
