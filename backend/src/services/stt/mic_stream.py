7144017eed292d9ca5bd08c36d6098d0d106de6f
JosephBec <150413350+JosephBec@users.noreply.github.com>
2026-01-14 23:11:05 -0800

    lightly modified the starter code to play the test wav file

--- backend/src/services/stt/mic_stream.py
+++ backend/src/services/stt/mic_stream.py
@@ -7,18 +7,13 @@ our USB microphone that we can then use with the Google STT API.
 """PyAudio Example: Play a wave file."""
 
 import wave
-import sys
-
+# import sys        # I shouldn't need this I think
 import pyaudio
 
-
+FILE_PATH = r"D:\Desktop\School Items\PSU Year 3\Capstone - CS-469\AI-Fishbowl\backend\src\services\stt\Test\test_audio.wav"
 CHUNK = 1024
 
-if len(sys.argv) < 2:
-    print(f'Plays a wave file. Usage: {sys.argv[0]} filename.wav')
-    sys.exit(-1)
-
-with wave.open(sys.argv[1], 'rb') as wf:
+with wave.open(FILE_PATH, 'rb') as wf:
     # Instantiate PyAudio and initialize PortAudio system resources (1)
     p = pyaudio.PyAudio()
 
@@ -28,9 +23,13 @@ with wave.open(sys.argv[1], 'rb') as wf:
                     rate=wf.getframerate(),
                     output=True)
 
+    print(f"Playing: {FILE_PATH}")
+
     # Play samples from the wave file (3)
     while len(data := wf.readframes(CHUNK)):  # Requires Python 3.8+ for :=
         stream.write(data)
+
+    print("Finished playback.")
 
     # Close stream (4)
     stream.close()
