118aae2d162ce997cac89f0d829ddc32269ffb28
HenryMcDowell <hmcdo2@pdx.edu>
2026-02-26 19:51:45 -0800

    added buffer and device check

--- backend/src/services/tts/tts_functions.py
+++ backend/src/services/tts/tts_functions.py
@@ -3,6 +3,7 @@ import numpy as np
 import pyaudio
 from pocket_tts import TTSModel
 import sys
+import torch
 
 tts_model = None
 default_voice_state = None
@@ -13,16 +14,22 @@ def _load_model():
     
     if tts_model is None: #Only loads if not already loaded
         print("Loading Kyutai Pocket TTS model...", flush=True)
-        sys.stderr.flush()
+
+        device = "cuda" if torch.cuda.is_available() else "cpu" #checks if torch is available in cuba, otherwise uses cpu
+        print(f"Using device: {device}")
+
         tts_model = TTSModel.load_model()
-        print("Pocket TTS loaded.", flush=True)
-        sys.stderr.flush()
 
         print("Getting voice state for 'alba'", flush=True)
-        sys.stderr.flush()
+
         default_voice_state = tts_model.get_state_for_audio_prompt("alba") #Preset voice for Kyutai
+        
+        #Moves voice state to device if possible
+        for key, value in default_voice_state.items():
+            if isinstance(value, torch.Tensor):
+                default_voice_state[key] = value.to(device)
+        
         print("Voice state loaded.", flush=True)
-        sys.stderr.flush()
 
 def _play_audio_stream(sample_rate: int):
     p = pyaudio.PyAudio()
@@ -46,11 +53,16 @@ def speak_text(text: str, personality_id: str = None):
     start_time = time.monotonic()
 
     print("Streaming TTS")
+
+    chunks = tts_model.generate_audio_stream(default_voice_state, text) #pre-buffer to fix stutter
+    buffer = [next(chunks) for _ in range(3)]
+
     stream, p = _play_audio_stream(tts_model.sample_rate)
+
     chunk_count = 0
 
     try:
-        for chunk in tts_model.generate_audio_stream(default_voice_state, text):
+        for chunk in buffer + list(chunks): #buffer
             audio = chunk.cpu().numpy()
             _write_audio_chunk(stream, audio)
             chunk_count += 1
